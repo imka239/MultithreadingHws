517b3c885c5d1237faefad3bc1bec7c765c07245
imka239 <ima2ima3go9@gmail.com>
2020-03-23 18:38:25 +0300

    bugs for build-jar fixed

--- java-solutions/ru/ifmo/rain/gnatyuk/implementor/build-jar.cmd
+++ java-solutions/ru/ifmo/rain/gnatyuk/implementor/build-jar.cmd
@@ -5,7 +5,7 @@ SET mod_dir=ru\ifmo\rain\gnatyuk\implementor
 SET mod_name=implementor
 
 
-SET root_path=%cd%\..\..\..\..\..\..\..\..\java-advanced-2020
+SET root_path=%cd%\..\..\..\..\..\..\..\java-advanced-2020
 SET mod_path=%root_path%\artifacts;%root_path%\lib
 
 SET src=%cd%\..\..\..\..\..
